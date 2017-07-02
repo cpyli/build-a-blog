15cfcb23b6ae7bd49db49486c43e7cc34a05d876
cpyli <cpyli@uw.edu>
2017-07-02 10:16:33 -0700

    Last update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:helloworld@localhost:8889/build-a-blog'
